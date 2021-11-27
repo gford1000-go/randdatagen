b60c0b13ab5f92a2a7e9c4d130dd147d8197901e
Geoff Ford <gford1000@hotmail.com>
2021-11-27 16:39:47 +0000

    Added basic tests

---
+++ generator_test.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGenerator(t *testing.T) {
+
+	var builder strings.Builder
+	var recordCount = 5
+
+	g := NewGenerator(recordCount, "GB")
+
+	err := g.Create(&builder)
+	if err != nil {
+		t.Fatalf("unexpected error - got %v", err)
+	}
+
+	records := strings.Split(builder.String(), "\n")
+	if len(records) != recordCount {
+		t.Fatalf("Invalid number of records: expected %v, got %v", recordCount, len(records))
+	}
+
+	for i, record := range records {
+		if n := len(strings.Split(record, ",")); n != 11 {
+			t.Fatalf("Invalid number of columns in record %v: expected %v, got %v", i, 11, n)
+		}
+	}
+}
